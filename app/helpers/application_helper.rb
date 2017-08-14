783a99442715ec115410ca1a4ba2ba0f3e3df03f
James Cross <jcross2774@gmail.com>
2017-08-13 23:16:49 -0600

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,10 +59,16 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
-end 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
 
+    if alert
+      alert_generator alert
+    end
+  end
 
+  def alert_generator msg
+    js add_gritter(msg, title: "James Cross Portfolio", sticky: false)
+  end
 
-
-
-
+end
