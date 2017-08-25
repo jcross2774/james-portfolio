d4e89ece895bf7f7f9f2ce8c2f33799f8d901eb7
James Cross <jcross2774@gmail.com>
2017-08-24 21:18:30 -0600

    Built out initial form and partial call for comments alog with controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.build(comment_params)
+  end
+
+  def comment_params
+    params.requier(:comment).permit(:comtent)
+  end
 end
